a0efd54a46359a2cb2d62fe56eabfb6e4967e814
Robert Wagner <rblick@duke.edu>
2010-02-26 18:40:30 +0000

    forProd now includes transparent reconnect support that was added to Dev and Test

--- forTest/IGSPnet_Cookie_Config.h
+++ forTest/IGSPnet_Cookie_Config.h
@@ -2,6 +2,7 @@
 #define IGSPNET_COOKIE_CONFIG
 
 /* path to Unix domain socket */
+//#define SOCKET_PATH "/tmp/cookieDaemon.sock"
 #define SOCKET_PATH "/var/www/bin/cookieDaemon.sock"
 /* Oracle connect string */
 #define DB_CONN_STRING "***REMOVED***"
