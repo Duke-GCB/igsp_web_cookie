a3f0225fdaefd91dfc0c2e58a9aca0012308d3db
Dan Leehr <dan.leehr@duke.edu>
2015-05-12 11:55:52 -0400

    Increase RSA_HEX_SIG_SIZE to handle larger (4096-bit) keys
    Otherwise keys overflow the buffers

--- src/IGSPnet_Cookie_Streamer.h
+++ src/IGSPnet_Cookie_Streamer.h
@@ -9,19 +9,19 @@
  * Description : Provides methods for building and parsing cookie contents
  *
  * Method Index: static int buildCookie(const char * userID, const char * dukey,
- *                  const char * IP, const char * cookieVersion, 
+ *                  const char * IP, const char * cookieVersion,
  *                  const char * clientID, char * cookieString) - constructs
  *                  cookie from first 5 arguments and places in 6th arg.
  *                  Requires cookieString buffer to be preallocated.
  *                  Returns 0.
  *               static int parseCookie(char * cookieString, char * userID,
- *                  char * dukey, char * IP, char * cookieVersion, 
+ *                  char * dukey, char * IP, char * cookieVersion,
  *                  char * clientID) - parses contents of cookieString and
  *                  places components in remaining 5 arguments (whose buffers
  *                  must be preallocated).  Note that this method destroys
  *                  cookieString.  Returns 0 if successful or -1
  *                  if cookieString cannot be parsed.
- *               static int buildSignedCookie(const char * cookie, 
+ *               static int buildSignedCookie(const char * cookie,
  *                  const char * sig, char * signedCookie) - combines
  *                  cookie and signature to produce signedCookie (whose
  *                  buffer must be preallocated).  Returns 0.
@@ -40,9 +40,9 @@ class IGSPnet_Cookie_Streamer
       static int parseCookie(char * cookieString, char * userID, char * dukey, char * IP, char * cookieVersion, char * clientID);
       static int buildSignedCookie(const char * cookie, const char * sig, char * signedCookie);
       static int parseSignedCookie(char * signedCookie, char * cookie, char * sig);
-/* Emperically, it appears that the length of the binhex-coded RSA signature 
- * is 4X the length of the private key.  So, for a 2048-bit key, the binhex
- * sig may be 512 characters long.
+/* Empirically, it appears that the length of the binhex-coded RSA signature
+ * is 4X the length of the private key.  So, for a 4096-bit key, the binhex
+ * sig may be 1024 characters long.
  * We know the max length of the cookie is 12+2+1+2+15+2+1+2+4 = 41.
  * Let's define some conservative constants, below:
  * IGSPNET_COOKIE_SIZE
@@ -51,7 +51,7 @@ class IGSPnet_Cookie_Streamer
       /* unsigned portion of IGSPnet cookie max size */
       static const int IGSPNET_COOKIE_SIZE = 50;  //overestimate
       /* max lenth of hex RSA sig */
-      static const int RSA_HEX_SIG_SIZE = 640;  //overestimate
+      static const int RSA_HEX_SIG_SIZE = 1280;  //overestimate
 };
 
 #endif
